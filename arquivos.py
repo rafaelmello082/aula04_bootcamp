f58a98b9c6494d434c014f16018e26acffb06e4a
rafaelmello082 <rafaelmello082@gmail.com>
2026-04-07 21:25:55 -0300

    arquivos.py

---
+++ arquivos.py
@@ -0,0 +1,11 @@
+import csv
+
+caminho_arquivo: str = 'exemplo.csv'
+arquivo_csv: list = []
+
+with open(file=caminho_arquivo, mode='r', encoding='utf-8') as arquivo:
+  leitor_csv = csv.DictReader(arquivo)
+  for linha in leitor_csv:
+    arquivo_csv.append(linha)
+
+print(arquivo_csv)
